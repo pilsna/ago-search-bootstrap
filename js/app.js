1b1cf777052b93ad92d3b3588502dc18abb8899e
Nils Rydh <nils.rydh@gmail.com>
2014-04-08 13:40:59 +0200

    figured out swipe, now activating dropdowns...

--- js/app.js
+++ js/app.js
@@ -1,90 +1,109 @@
  require([
-        "esri/arcgis/utils",
+         "esri/arcgis/utils",
          "esri/map",
          "esri/dijit/Scalebar",
          "esri/geometry/Extent",
          "esri/layers/WebTiledLayer",
+         "esri/dijit/LayerSwipe",
          "dojo/dom",
+         "dojo/on",
          "./js/bootstrapmap.js",
          "dojo/domReady!",
      ],
-     function(arcgisUtils, Map, Scalebar, Extent, WebTiledLayer, dom, BootstrapMap) {
-         <!-- Get a reference to the ArcGIS Map class -->
+     function(arcgisUtils, Map, Scalebar, Extent, WebTiledLayer, LayerSwipe, dom, on, BootstrapMap) {
          var map = null;
-         /* BootstrapMap.create("mapDiv", {
-             basemap: 'gray',
-             center: [-117.1, 33.6],
-             zoom: 9
-         }); */
+         var operationalLayers = null;
+         var swipeWidget = null;
+
          var deferred = arcgisUtils.createMap("8b3ce9af79724f30a9f924c7bca1d339", "mapDiv").then(function(response) {
-             //update the app 
+             map = response.map;
              $("#title").text(response.itemInfo.item.title);
              $("#subtitle").text(response.itemInfo.item.snippet);
+             operationalLayers = response.itemInfo.itemData.operationalLayers;
+             console.log(response.itemInfo.itemData.operationalLayers);
+             //map.removeLayer(response.itemInfo.itemData.operationalLayers[1]);
+             //map.removeLayer(response.itemInfo.itemData.operationalLayers[0]);
+             //operationalLayers[0].layerObject.hide();
+             //operationalLayers[1].layerObject.hide();
 
-             map = response.map;
+             console.log(operationalLayers);
+
+            swipeWidget = new LayerSwipe({
+                 type: "vertical", //Try switching to "scope" or "horizontal"
+                 map: map,
+                 layers: [operationalLayers[1].layerObject]
+             }, "swipeContainer");
+             swipeWidget.startup();
+             on(swipeWidget, 'swipe', function(layers) {
+                 console.log(layers);
+             });
          });
-         
+        var setSwipelayer = function(layer){
+            swipeWidget.layers = [layer];
+        }
          // 8b3ce9af79724f30a9f924c7bca1d339
+         var switchToBasemap = function(name) {
+             var l, options;
+             switch (name) {
+                 case "Water Color":
+                     options = {
+                         id: 'Water Color',
+                         copyright: 'stamen',
+                         resampling: true,
+                         subDomains: ['a', 'b', 'c', 'd']
+                     };
+                     l = new WebTiledLayer('http://${subDomain}.tile.stamen.com/watercolor/${level}/${col}/${row}.jpg', options);
+                     map.addLayer(l);
+                     break;
+
+                 case "MapBox Space":
+                     options = {
+                         id: 'mapbox-space',
+                         copyright: 'MapBox',
+                         resampling: true,
+                         subDomains: ['a', 'b', 'c', 'd']
+                     };
+                     l = new WebTiledLayer('http://${subDomain}.tiles.mapbox.com/v3/eleanor.ipncow29/${level}/${col}/${row}.jpg', options);
+                     map.addLayer(l);
+                     break;
+
+                 case "Pinterest":
+                     options = {
+                         id: 'mapbox-pinterest',
+                         copyright: 'Pinterest/MapBox',
+                         resampling: true,
+                         subDomains: ['a', 'b', 'c', 'd']
+                     };
+                     l = new WebTiledLayer('http://${subDomain}.tiles.mapbox.com/v3/pinterest.map-ho21rkos/${level}/${col}/${row}.jpg', options);
+                     map.addLayer(l);
+                     break;
+                 case "Streets":
+                     map.setBasemap("streets");
+                     break;
+                 case "Imagery":
+                     map.setBasemap("hybrid");
+                     break;
+                 case "National Geographic":
+                     map.setBasemap("national-geographic");
+                     break;
+                 case "Topographic":
+                     map.setBasemap("topo");
+                     break;
+                 case "Gray":
+                     map.setBasemap("gray");
+                     break;
+                 case "Open Street Map":
+                     map.setBasemap("osm");
+                     break;
+             }
+         }
 
          $(document).ready(function() {
              $("#basemapList li").click(function(e) {
-                 var l, options;
-                 BootstrapMap.clearBaseMap(map);
-                 switch (e.target.text) {
-                     case "Water Color":
-
-
-                         options = {
-                             id: 'Water Color',
-                             copyright: 'stamen',
-                             resampling: true,
-                             subDomains: ['a', 'b', 'c', 'd']
-                         };
-                         l = new WebTiledLayer('http://${subDomain}.tile.stamen.com/watercolor/${level}/${col}/${row}.jpg', options);
-                         map.addLayer(l);
-                         break;
-
-                     case "MapBox Space":
-
-                         options = {
-                             id: 'mapbox-space',
-                             copyright: 'MapBox',
-                             resampling: true,
-                             subDomains: ['a', 'b', 'c', 'd']
-                         };
-                         l = new WebTiledLayer('http://${subDomain}.tiles.mapbox.com/v3/eleanor.ipncow29/${level}/${col}/${row}.jpg', options);
-                         map.addLayer(l);
-                         break;
-
-                     case "Pinterest":
-                         options = {
-                             id: 'mapbox-pinterest',
-                             copyright: 'Pinterest/MapBox',
-                             resampling: true,
-                             subDomains: ['a', 'b', 'c', 'd']
-                         };
-                         l = new WebTiledLayer('http://${subDomain}.tiles.mapbox.com/v3/pinterest.map-ho21rkos/${level}/${col}/${row}.jpg', options);
-                         map.addLayer(l);
-                         break;
-                     case "Streets":
-                         map.setBasemap("streets");
-                         break;
-                     case "Imagery":
-                         map.setBasemap("hybrid");
-                         break;
-                     case "National Geographic":
-                         map.setBasemap("national-geographic");
-                         break;
-                     case "Topographic":
-                         map.setBasemap("topo");
-                         break;
-                     case "Gray":
-                         map.setBasemap("gray");
-                         break;
-                     case "Open Street Map":
-                         map.setBasemap("osm");
-                         break;
-                 }
+                 map.removeAllLayers();
+                 switchToBasemap(e.target.text);
+                 map.addLayers([operationalLayers[1].layerObject, operationalLayers[0].layerObject]);
+                 setSwipelayer(operationalLayers[0].layerObject);
              });
              var addresses = new Bloodhound({
                  name: 'ago-geocode',
@@ -104,18 +123,12 @@
              });
 
              addresses.initialize();
-             /*addresses.get('Sweden', function(suggestions) {
-              console.log(suggestions);
-                $(suggestions).each(function(suggestion) {
-                    console.log(suggestion);
-                });
-              });*/
+
              $('.typeahead').typeahead(null, {
                  name: 'text',
                  displayKey: 'text',
                  source: addresses.ttAdapter()
              }).on('typeahead:selected', function($e, datum) {
-                 //http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?text="<text1>"&magicKey="<magicKey1>"&f=json
                  var url = 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?text=' + datum.text + '&magicKey=' + datum.magicKey + '&f=json';
                  $.get(url, function(data) {
                      var result = JSON.parse(data);
